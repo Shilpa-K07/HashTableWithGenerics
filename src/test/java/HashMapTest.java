e0e5e318b855c841095c1cd2e41c47e0853d2a87
shilpa <shilpa07udupi@gmail.com>
2020-11-09 23:28:17 +0530

    feat: find word frequency in a large sentence

--- src/test/java/HashMapTest.java
+++ src/test/java/HashMapTest.java
@@ -37,4 +37,44 @@ public class HashMapTest {
 		System.out.println(hashMap);
 		Assert.assertNotEquals(3, frequency);
 	}
+	
+	@Test
+	public void givenSentenceWhenWordsAreAddedToListShouldReturnParanoidFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep putting themselves deliberately into paranoid "
+				+ "avoidable situations";
+		LinkedHashMapImplementation<String, Integer> hashMap = new LinkedHashMapImplementation<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for(String word : words) {
+			Integer value = hashMap.get(word);
+			if(value == null)
+				value = 1;
+			else
+				value = value + 1;
+			hashMap.add(word, value);
+		}
+		System.out.println(hashMap);
+		int frequency = hashMap.get("paranoid");
+		Assert.assertEquals(3, frequency);
+	}
+	
+	@Test
+	public void givenSentenceWhenWordsAreAddedToListShouldPassFalseTest() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep putting themselves deliberately into paranoid "
+				+ "avoidable situations";
+		LinkedHashMapImplementation<String, Integer> hashMap = new LinkedHashMapImplementation<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for(String word : words) {
+			Integer value = hashMap.get(word);
+			if(value == null)
+				value = 1;
+			else
+				value = value + 1;
+			hashMap.add(word, value);
+		}
+		System.out.println(hashMap);
+		int frequency = hashMap.get("paranoid");
+		Assert.assertNotEquals(4, frequency);
+	}
 }
